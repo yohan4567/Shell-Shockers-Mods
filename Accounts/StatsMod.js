14bde6fa8196318cc549cbcd30f3cec17196a8ae
Agent Adam <88847241+AgentAdam5972@users.noreply.github.com>
2022-08-15 20:16:31 +0100

    Update StatsMod.js

--- Accounts/StatsMod.js
+++ Accounts/StatsMod.js
@@ -2,5 +2,6 @@
     ["894913", "Agent Adam ツ#5972", "691644457287811152", "Creator"],
     ["45968679", "condsicc 盗む#5007", "714391556295229514", "Mask League Events"],
     ["25265181", "Dines#5711", "734386698418651288", "Mask League Events"],
-    ["48817164", "hygge#2314", "842967475503104020", "Mask League Events"]
+    ["48817164", "hygge#2314", "842967475503104020", "Mask League Events"],
+    ["6337", "Hazard#6186", "630043925012283441", "Personal Usage"]
 ]
