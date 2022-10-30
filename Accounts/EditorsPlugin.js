28e9d28a17f8225f82e3c608f1d819494a0bf1dc
Agent Adam <88847241+AgentAdam5972@users.noreply.github.com>
2022-10-30 07:53:10 +0000

    Update EditorsPlugin.js

--- Accounts/EditorsPlugin.js
+++ Accounts/EditorsPlugin.js
@@ -3,5 +3,6 @@
   ["mAxzQKLYtjQeH1rryFTCORiaTkl2", "[Sh6rk#0453 / 696100580816322651]"],
   ["7zcv0nmU4HUpm9vgTl9YFezRr3q2", "[Yaboi Jenkins#1420 / 447156904553086987]"],
   ["QN8s7Me4N0Sykmma0jioQ9PHkW52", "[yaz.fx#9758 / 927405377824374795]"],
-  ["wuMpzLGfFeauI4YMZgutXMzBiLU2", "[Fishy#8335 / 1014744655373541396]"]
+  ["wuMpzLGfFeauI4YMZgutXMzBiLU2", "[Fishy#8335 / 1014744655373541396]"],
+  ["EIFbsek1xEaWJTdGYdG5tUWxEe32", "[Bubbleaxe#2803 / 783351635808092190]"]
 ]
